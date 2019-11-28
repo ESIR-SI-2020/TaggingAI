7c9b55258376ce6ce388c41d021af806aa653adf
agegout <adrien.gegout@gmail.com>
2019-11-28 16:03:15 +0100

    added simple models for english to the programm. Added execution context path for the models loaders in language_detector and in tagger_model. Added multi language support for theorical futur language in the main

--- ia_event_handler_python/Main.py
+++ ia_event_handler_python/Main.py
@@ -15,24 +15,32 @@ results = my_parser.initParser()
 logger = my_logger.initLogger(results.file_log_debug, results.console_log_debug)
 
 # Generating Kafka consumer
-kafka_consumer = kafka.KafkaConsumer(
-    results.topic,
-    bootstrap_servers=[results.kafka_producer],
-    auto_offset_reset='latest',
-    enable_auto_commit=True,
-    group_id=results.group_id,
-    value_deserializer=lambda x: json.loads(x.decode('utf-8'))
-)
-logger.debug("Kafka consumer initialized, with parameters ==> kafka_topic = ' " + results.topic + " ', kafka_producer = ' " +
-            results.kafka_producer + " ' , consumer_group_id = ' " + results.group_id + " '.")
+#kafka_consumer = kafka.KafkaConsumer(
+#    results.topic,
+#    bootstrap_servers=[results.kafka_producer],
+#    auto_offset_reset='latest',
+#    enable_auto_commit=True,
+#    group_id=results.group_id,
+#    value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+#)
+logger.debug("Kafka consumer initialized, with parameters ==> kafka_topic = ' " + str(results.topic) + " ', kafka_producer = ' " +
+            str(results.kafka_producer) + " ' , consumer_group_id = ' " + str(results.group_id) + " '.")
 
 # We initialize the model to None, because they are heavy and we don't know if they will be all used
-modelTaggingEn = None
-modelTaggingFr = None
-labels_predited = None
+modelTaggingDict = {}
+
 
 # Main loop of the program
-for message in kafka_consumer :
+#for message in kafka_consumer :
+TEST = []
+TEST.append({'articleUrl' : 'https://www.theguardian.com/us-news/2019/nov/27/trump-hong-kong-bills-signed-china-protest'})
+TEST.append({'articleUrl' : 'https://www.theguardian.com/commentisfree/2019/nov/28/tour-world-massive-attack-band-climate'})
+TEST.append({'articleUrl' : 'https://www.theguardian.com/football/blog/2019/nov/28/dries-mertens-napoli-rivals-liverpool-flaws'})
+TEST.append({'articleUrl' : 'https://www.theguardian.com/lifeandstyle/2019/nov/28/limpy-the-turkey-thanksgiving-reading-massachusetts'})
+TEST.append({'articleUrl' : 'https://www.theguardian.com/lifeandstyle/2019/nov/24/i-live-as-a-feminist-but-im-tired-of-being-so-furious-all-the-time-mariella-frostrup'})
+TEST.append({'articleUrl' : 'https://www.theguardian.com/lifeandstyle/2019/nov/24/how-to-blow-your-nose-properly'})
+
+for message in TEST :
     try:
         logger.debug("kafka message received on topic ' " + results.topic + " '")
         logger.debug("  - message content : ' " + str(message) + " '.")
@@ -46,24 +54,20 @@ for message in kafka_consumer :
             logger.debug("Article language is " + article_lang)
 
             # The models are heavy, so we load them the first time a specific language model is needed
-            if (article_lang is "en" and modelTaggingEn is None) :
-                modelTaggingEn = tagger_model.load_model("","",logger)
-            if (article_lang is "fr" and modelTaggingFr is None) :
-                modelTaggingFr = tagger_model.load_model("","",logger)
+            if article_lang not in modelTaggingDict :
+                modelTaggingDict[article_lang] = tagger_model.load_model("model_" + article_lang + ".json", "model_" + article_lang + ".h5", logger)
 
             # We prepare the text, because we can't feed raw text to the model
             article_text_prepared = tagger_model.prepare_text(article_text)
             
             # We used the correct model for the detected language, to get the predicted labels
-            if (article_lang is "en") :
-                labels_predited = tagger_model.predict_labels(article_text_prepared, modelTaggingEn)
-            elif (article_lang is "fr") :
-                labels_predited = tagger_model.predict_labels(article_text_prepared, modelTaggingFr)
+            labels_predited = tagger_model.predict_labels(article_text_prepared, modelTaggingDict[article_lang])
 
             logger.debug("Label(s) '" + str(labels_predited) + "' detected for the kafka message : " + str(message))
             
         else :
+            
             logger.critical("Article url not found in kafka message :" + str(message))
 
     except Exception as identifier:
-        logger.critical("Error for kafka message : ' " + str(message) + " ', error is " + identifier)
+        logger.critical("Error for kafka message : ' " + str(message) + " ', error is " + str(identifier))
--- ia_event_handler_python/tagger_model.py
+++ ia_event_handler_python/tagger_model.py
@@ -3,18 +3,21 @@ from keras.layers import Dense
 from keras.models import model_from_json
 import numpy
 import os
+import sys
 import logging
 
 # Function to load keras model saved into json and h5. return the fully loaded model
 def load_model(model_json_name, model_h5_name, logger) :
     # Open saved model file
-    json_file = open(model_json_name, 'r')
+    executionFile = sys.argv[0]
+    pathName = os.path.dirname(executionFile)
+    json_file = open(pathName + "/modelTagging/" + model_json_name, 'r')
     loaded_model_json = json_file.read()
     json_file.close()
     # load model skeleton
     loaded_model = model_from_json(loaded_model_json)
     # load weights into new model
-    loaded_model.load_weights(model_h5_name)
+    loaded_model.load_weights(pathName + "/modelTagging/" + model_h5_name)
 
     logger.debug("Model " + model_json_name + "/" + model_h5_name + " loaded")
 
