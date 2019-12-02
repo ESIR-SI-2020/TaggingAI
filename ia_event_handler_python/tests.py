cd859a8e5968193c7a90358b6912d748fdfe9037
Moisan Simon <simon.moisan@etudiant.univ-rennes1.fr>
2019-12-02 10:29:31 +0100

    Add first test on my_parser

--- ia_event_handler_python/tests.py
+++ ia_event_handler_python/tests.py
@@ -1,4 +1,11 @@
 import unittest
+import language_detector
+import Main
+import my_logger
+import m_parser
+import web_scrapper
 
 class IAeventHandlerTest(unittest.TestCase) :
     
+    def testParser(self) :
+        
