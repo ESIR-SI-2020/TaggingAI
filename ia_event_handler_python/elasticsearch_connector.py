e939cff52100993b6b286920c7cb811f355641e1
agegout <adrien.gegout@gmail.com>
2020-01-30 12:20:20 +0100

    WIP elasticsearch connector

--- ia_event_handler_python/elasticsearch_connector.py
+++ ia_event_handler_python/elasticsearch_connector.py
@@ -13,8 +13,9 @@ def updateSuggestedTags(article_url, predicted_tag, ESConnector, logger):
     try:
         # This query is for elasticsearch 5.6 !
         res = ESConnector.search(index='Article',body={'query':{'match_all':{}},
-            "_source": ["article_url", "article_url"]})
+            "_source": ["article_url", article_url]})
         logger.debug("")
+
     except Exception as error:
         logger.critical("Error with Elasticsearch connector : " + str(error))
         raise RuntimeError("Error with Elasticsearch connector : " + str(error))
